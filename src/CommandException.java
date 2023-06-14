72616a7c7eaa8e75719021bdf8f79712abe9b952
emirkaramoldoev <emirkaramoldoev1@gmail.com>
2023-06-14 22:18:31 +0600

    Внесены дополнения

--- src/CommandException.java
+++
@@ -1,8 +0,0 @@
-import java.util.InputMismatchException;
-
-public class CommandException extends InputMismatchException {
-
-    public CommandException(String message) {
-        super(message);
-    }
-}
